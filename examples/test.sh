b2f78bc66b9d4d738f21c5d0d7b606a49fb22bde
Jerry Lundström <lundstrom.jerry@gmail.com>
2024-05-22 12:32:38 +0200

    Update
    - Fix README
    - Change `stat_periodic` to `stats_periodic`
    - Change `stat_sum` to `stats_sum`
    - Add description to all properties
    - `header`: Remove `time_units_per_sec` from required
    - `stats`:
      - Rename `requests` to `queries`
      - Rename `answers` to `responses`
      - Rename `responses` to `response_rcodes`
      - Rename `answer_latency` to `response_latency`

--- examples/test.sh
+++ examples/test.sh
@@ -1,15 +1,6 @@
-# DNS Metrics JSON Schema
+#!/bin/sh
 
-A JSON schema to define DNS metrics that tools can use to generate compatible
-JSON metrics and statistics.
-
-## Test
-
-```
-pip install check-jsonschema
-cd examples
 check-jsonschema --schemafile ../dns-metrics.schema.json header.json
 check-jsonschema --schemafile ../dns-metrics.schema.json event.json
 check-jsonschema --schemafile ../dns-metrics.schema.json stats_periodic.json
 check-jsonschema --schemafile ../dns-metrics.schema.json stats_summary.json
-```
